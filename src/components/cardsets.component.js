d0ac0f85fdbdbb7c33838736dde947f97ea3d140
Annie Vo <avo2@unc.edu>
2023-03-24 12:40:27 -0400

    cardsets

--- src/components/cardsets.component.js
+++ src/components/cardsets.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './sign_.component.css';
 import { collection, doc, setDoc, addDoc } from "firebase/firestore";
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 
  
 export default class Cardsets extends Component {
@@ -15,18 +15,29 @@ export default class Cardsets extends Component {
       set_id: '',
       card_ids: ''
     }
-    this.set_Doc = this.set_Doc.bind(this);
-    this.set_with_merge = this.set_with_merge.bind(this);
-    this.data_types = this.data_types.bind(this);
+    // this.setDoc = this.setDoc.bind(this);
+    // this.set_with_merge = this.set_with_merge.bind(this);
+    // this.data_types = this.data_types.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
   }
-
+  async handleSubmit(event) {
+    console.log("test");
+    event.preventDefault();
+    this.setDoc();
+  }
   // Add a new document with a generated i
-set_Doc(event) {
-  const docRef = addDoc(collection("English", "sets"), {
+  
+setDoc = async () => {
+  try {
+  const docRef = await addDoc(collection(db, "sets"), {
     sides: 3,
   });
   console.log("The cardset ID: ", docRef.id);  
+} catch(error) {
+  console.log(error.message);
+  alert(error.message);
+}
 };
 
   render() {
@@ -34,7 +45,7 @@ set_Doc(event) {
 
       <div>
           <h1>Cardsets</h1>
-          <form className="button" onSubmit = {this.setDoc}>
+          <form className="button" onSubmit = {this.handleSubmit}>
           <input type ="submit" value="test"></input>
           </form>
       </div>
